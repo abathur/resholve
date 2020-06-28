51e305c5f489d75ba9b6a3fee7f6a922a9a01d96
Travis A. Everett <travis.a.everett@gmail.com>
2020-06-28 17:22:43 -0500

    rm dead code

--- mk-resholved-derivation.nix
+++ mk-resholved-derivation.nix
@@ -8,8 +8,6 @@ let
     // {
       inherit pname version src;
       buildInputs = [ resholved ];
-      # tentatively disabled because gc probably knows things I don't :)
-      # propagatedBuildInputs = inputs;
       RESHOLVE_PATH = "${lib.makeBinPath inputs}";
       RESHOLVE_ALLOW = toString
         (lib.mapAttrsToList (name: value: map (y: name + ":" + y) value) allow);
