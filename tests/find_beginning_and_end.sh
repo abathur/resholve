3bd1b01546bc34ff5258adeb3cdca859697bb1d9
Travis A. Everett <travis.a.everett@gmail.com>
2020-07-03 20:11:58 -0500

    [WIP] inject code before/after script

---
+++ tests/find_beginning_and_end.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+
+# I'm just a small file
+
+		# with some clumsy attempts
+
+	# to ensure resholved finds the right
+	# before/after points to inject code
+{
+	echo one
+	echo two
+} > /dev/null
+# lorem
+
+		# ipsum
+
+	# dolor
+	# sit
---
+++ tests/find_beginning_and_end2.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+
+# I'm just a small file
+
+		# with some clumsy attempts
+
+	# to ensure resholved finds the right
+	# before/after points to inject code
+for number in one two three; do
+	echo $number
+done
+
+
+# lorem
+
+		"$0" # ipsum
+
+	# dolor
+	# sit
