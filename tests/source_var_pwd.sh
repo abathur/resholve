cb1e7e17eae8ccc32e7d71fa4dc86d71dd8a1c6f
Travis A. Everett <travis.a.everett@gmail.com>
2021-02-21 15:35:56 -0600

    fix $V/${V} normalization in keep sub:var

--- tests/source_var_pwd.sh
+++ tests/source_var_pwd.sh
@@ -1,5 +1,6 @@
 # fails because $PWD requires a dynamic parse
 # (I can resolve from a dictionary but haven't
 # seen a clear reason to do it...)
-# and isn't exempted with --allow source:PWD
+# and isn't exempted with --keep source:PWD
 source $PWD/file_simple.sh
+source ${PWD}/file_simple.sh
