bb502aebb429c1244a1a9ee64a90084de5f9e115
Travis A. Everett <travis.a.everett@gmail.com>
2021-09-10 17:55:10 -0500

    update source.nix

--- source.nix
+++ source.nix
@@ -3,7 +3,7 @@
 }:
 
 rec {
-  version = "0.6.0-rc.4";
+  version = "0.6.0-rc.5";
   rSrc =
     # local build -> `make ci`; `make clean` to restore
     # return to remote source
@@ -14,6 +14,6 @@ rec {
         owner = "abathur";
         repo = "resholve";
         rev = "v${version}";
-        hash = "sha256-WWG3+wHQAAnw+62+IYeNdigOcGkgDezatbSwOI93pn8=";
+        hash = "sha256-n7l2vTYEw7ehSQ9wW1Stux3seLrpKtVKg9xCEMpykR4=";
       };
 }
