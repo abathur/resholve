1e98669761ecb0d0741a6da0f73c8988b3b5497d
Travis A. Everett <travis.a.everett@gmail.com>
2021-09-18 13:27:09 -0500

    extract nix util funcs & stub resholveScript*
    The existing organization of the nix functions for validating input
    and constructing resholve's CLI invocation is a minor impediment to
    others remixing resholve into more and more nix/nixpkgs contexts.
    
    This:
    - extracts those out into resholve-utils.nix
    - stubs out a basic resholveScript and resholveScriptBin
    - adds trivial tests for each to resholve's test/CI suite

--- default.nix
+++ default.nix
@@ -12,8 +12,54 @@ with pkgs; rec
     inherit (deps) binlore;
     inherit (deps.oil) oildev;
   };
-  resholvePackage = callPackage ./resholve-package.nix {
+  resholve-utils = callPackage ./resholve-utils.nix {
     inherit resholve;
     inherit (deps) binlore;
   };
+  resholvePackage = callPackage ./resholve-package.nix {
+    inherit resholve;
+    inherit resholve-utils;
+  };
+  resholveScript = name: partialSolution: body:
+    writeTextFile {
+      inherit name;
+      executable = true;
+      text = ''
+        #!${runtimeShell}
+        ${body}'';
+      checkPhase = ''
+        (
+          PS4=$'\x1f'"\033[33m[resholve context]\033[0m "
+          set -x
+          ${resholve-utils.makeInvocation name (partialSolution // {
+            scripts = [ "${placeholder "out"}" ];
+            interpreter = "${runtimeShell}";
+          })}
+        )
+        ${stdenv.shell} -n $out
+      '';
+    };
+  # writeResholveBin "foo" { ... } '' echo "Hello" ''
+  resholveScriptBin = name: partialSolution: body:
+    writeTextFile rec {
+      inherit name;
+      executable = true;
+      destination = "/bin/${name}";
+      text = ''
+        #!${runtimeShell}
+        ${body}'';
+      checkPhase = ''
+        (
+          cd "$out"
+          PS4=$'\x1f'"\033[33m[resholve context]\033[0m "
+          set -x
+          : changing directory to $PWD
+          ${resholve-utils.makeInvocation name (partialSolution // {
+            scripts = [ "bin/${name}" ];
+            interpreter = "${runtimeShell}";
+          })}
+        )
+        ${stdenv.shell} -n $out/bin/${name}
+      '';
+    };
 }
