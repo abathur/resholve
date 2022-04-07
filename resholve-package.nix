8725e9b751ce42eca78879f7ffb6bc882de2a1fc
Travis A. Everett <travis.a.everett@gmail.com>
2022-04-06 22:59:46 -0500

    nix: split build to collect lore pre-resholve
    Split resholvePackage up to build the underlying package by itself
    and then separately build the resholved version. This enables us to
    collect lore for the base package and use it if we need to also
    resholve intra-package references. Builds on 86dbf76.

--- resholve-package.nix
+++ resholve-package.nix
@@ -10,10 +10,41 @@
 let
   inherit stdenv;
 
-  self = (stdenv.mkDerivation ((removeAttrs attrs [ "solutions" ])
+  /*
+  Knock out our special solutions arg, but otherwise
+  just build what the caller is giving us. We'll
+  actually resholve it separately below (after we
+  generate binlore for it).
+  */
+  unresholved = (stdenv.mkDerivation ((removeAttrs attrs [ "solutions" ])
     // {
     inherit pname version src;
-    buildInputs = (lib.optionals (builtins.hasAttr "buildInputs" attrs) attrs.buildInputs) ++ [ resholve ];
+  }));
+in
+/*
+resholve in a separate derivation; some concerns:
+- we aren't keeping many of the user's args, so they
+  can't readily set LOGLEVEL and such...
+- not sure how this affects multiple outputs
+*/
+lib.extendDerivation true passthru (stdenv.mkDerivation {
+    src = unresholved;
+    version = unresholved.version;
+    pname = "resholved-${unresholved.pname}";
+    buildInputs = [ resholve ];
+
+    # retain a reference to the base
+    passthru = unresholved.passthru // {
+      unresholved = unresholved;
+    };
+
+    # do these imply that we should use NoCC or something?
+    dontConfigure = true;
+    dontBuild = true;
+
+    installPhase = ''
+      cp -R $src $out
+    '';
 
     # enable below for verbose debug info if needed
     # supports default python.logging levels
@@ -29,9 +60,7 @@ let
         PS4=$'\x1f'"\033[33m[resholve context]\033[0m "
         set -x
         : changing directory to $PWD
-        ${builtins.concatStringsSep "\n" (resholve-utils.makeCommands solutions)}
+        ${builtins.concatStringsSep "\n" (resholve-utils.makeCommands solutions unresholved)}
       )
     '';
-  }));
-in
-lib.extendDerivation true passthru self
+  })
