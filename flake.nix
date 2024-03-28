7ca493c3e670622f4a0489d587cb22a91057c291
Travis A. Everett <travis.a.everett@gmail.com>
2024-03-28 14:44:54 -0500

    try testing again

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs/staging";
     nixpkgs.url = "github:abathur/nixpkgs/e7891ac2106700c183659882c3f5799bd1aa1c91";
-
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
@@ -74,7 +73,10 @@
               ${pkgs.ansifilter}/bin/ansifilter -o $out/nix-demo.txt --text nix-demo.ansi
             '';
           };
-          checks = {} // {
+          checks = pkgs.callPackages nixpkgs/test.nix {
+            inherit (pkgs) resholve;
+            rSrc = pkgs.lib.cleanSource self;
+          } // {
             aarch64-cross-test-lesspipe = pkgs.pkgsCross.aarch64-multiplatform.lesspipe.override (old: {
               inherit (pkgs.pkgsCross.aarch64-multiplatform) resholve;
             });
--- tests/parse_awk.sh
+++ tests/parse_awk.sh
@@ -1,3 +1,4 @@
 # 2 cases for issue 82
 awk -F'\t' -vcategory="$1" '{ split($9, cs, "|");for (i in cs) if (cs[i] == category) { print; break; }; }'
 awk -F'\t' -v category="$1" '{ split($9, cs, "|");for (i in cs) if (cs[i] == category) { print; break; }; }'
+awk 'BEGIN { system("date"); close("date")}'
