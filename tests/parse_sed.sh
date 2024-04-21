03a331cec8b5bb5cbb409fd6778216f630a1e607
Travis A. Everett <travis.a.everett@gmail.com>
2024-04-21 15:33:34 -0500

    ~handle sedparse errors

--- tests/parse_sed.sh
+++ tests/parse_sed.sh
@@ -1,9 +1,14 @@
 echo heh > infile
 sed -e 's/e/a/' infile
 # TODO: when you've got test scaffold support for error cases, add true e commands
-# sed -e 'e echo'
+# echo heh | sed -e 'e echo hah'
 echo bleh | sed 's/e/a/g'
 echo blah | sed 's/.*:/,/;s/$/,/'
 # throw away status because /mnt/etc
 # won't be available for the test
 sed -i "1p" /mnt/etc || true
+
+# extracted/simplified from pihole
+sed -E -e "s,.*(query\\[A|DHCP).*,${COL_NC}&${COL_NC},"
+# patched version of above that should actually parse
+sed -E -e 's,.*(query\[A|DHCP).*,'"${COL_NC}&${COL_NC},"
